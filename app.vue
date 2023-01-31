ccea40f6f0b0d41407c64eb803c161453bec6420
Thijs Jung <thijs.jung@gmail.com>
2023-01-31 23:00:37 +0100

    Add all elements for the journals page.

--- app.vue
+++ app.vue
@@ -1,5 +1,29 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      journals: [
+        { name: "Espanol", description: "Un sitio done dejar todos mis pensamientos espanoles." },
+        { name: "Dreams", description: "An overview of my subconsciousness, as presented to me during the night time." }
+      ]
+    }
+  }
+}
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <header>
+      Do Tell
+    </header>
+    <div>
+      <div>All journals</div>
+      <div>New</div>
+    </div>
+    <article v-for="journal in journals">
+      {{ journal.name }}
+      {{ journal.description }}
+      <div>Edit menu</div>
+    </article>
   </div>
 </template>
