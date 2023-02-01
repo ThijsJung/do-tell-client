75d25721536b2638fb56c9d18b7a57d2788f3d82
Thijs Jung <thijs.jung@gmail.com>
2023-02-01 17:01:53 +0100

    Add journal page.

---
+++ pages/journal/[journalId].vue
@@ -0,0 +1,54 @@
+<script lang="ts">
+export default {
+    data() {
+        return {
+            journal:
+                { id: 1, name: "Espanol", description: "Un sitio done dejar todos mis pensamientos espanoles." },
+            entries: [
+                { id: 1, title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" },
+                { id: 2, title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" },
+                { id: 3, title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" },
+                { id: 4, title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" },
+                { id: 5, title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" }
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="flex flex-col px-3 py-2">
+        <div class="flex flex-row">
+            <div class="grow">
+                <NuxtLink to="/">All journals</NuxtLink> > {{ journal.name }}
+            </div>
+            <div>
+                <button class="w-30 h-8 hover:bg-sky-200 cursor-pointer">
+                    <span class="material-symbols-outlined align-middle">
+                        add
+                    </span>New
+                    <span class="material-symbols-outlined align-middle">
+                        expand_more
+                    </span>
+                </button>
+            </div>
+        </div>
+
+        <article class="p-3 m-2 flex flex-col " v-for="entry in entries">
+            <div class="flex">
+                <div class="text-xl grow">
+                    {{ entry.title }}
+                </div>
+                <div>
+                    <span class="material-symbols-outlined w-8 h-8 hover:bg-sky-200 cursor-pointer">
+                        more_vert
+                    </span>
+                </div>
+            </div>
+            <div>
+                {{ entry.content }}
+            </div>
+        </article>
+        <p>{{ $route.params }}</p>
+    </div>
+</template>
