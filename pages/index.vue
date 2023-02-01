99ee68ffc9cbee60921117685653b8aea4edff49
Thijs Jung <thijs.jung@gmail.com>
2023-02-01 17:39:26 +0100

    Toggle new and edit buttons.

--- pages/index.vue
+++ pages/index.vue
@@ -14,13 +14,10 @@ export default {
 <template>
     <div class="flex flex-col px-3 py-2">
         <div class="flex flex-row">
-            <div class="grow">All journals</div>
+            <div class="basis-4/5">All journals</div>
             <div>
-                <button class="w-20 h-8 hover:bg-sky-200 cursor-pointer">
-                    <span class="material-symbols-outlined align-middle">
-                        add
-                    </span>New
-                </button>
+                <button class="cursor-pointer rounded border-white border-2 px-2">New</button>
+                <!-- <button class="w-20 h-8 hover:bg-sky-200 cursor-pointer">New</button> -->
             </div>
         </div>
         <article class="p-3 m-2 flex flex-col " v-for="journal in journals"
--- pages/journal/[journalId].vue
+++ pages/journal/[journalId].vue
@@ -10,7 +10,9 @@ export default {
                 { id: 3, title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" },
                 { id: 4, title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" },
                 { id: 5, title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" }
-            ]
+            ],
+            showEdit: false,
+            showNew: false
         }
     }
 }
@@ -19,18 +21,21 @@ export default {
 <template>
     <div class="flex flex-col px-3 py-2">
         <div class="flex flex-row">
-            <div class="grow">
+            <div class="basis-3/4">
                 <NuxtLink to="/">All journals</NuxtLink> > {{ journal.name }}
             </div>
             <div>
-                <button class="w-30 h-8 hover:bg-sky-200 cursor-pointer">
-                    <span class="material-symbols-outlined align-middle">
-                        add
-                    </span>New
+                <button class="cursor-pointer rounded border-white border-2 px-1" @click="showNew = !showNew">
+                    New
                     <span class="material-symbols-outlined align-middle">
                         expand_more
                     </span>
                 </button>
+                <div v-if="showNew" class="flex-col">
+                    <div><span class="material-symbols-outlined align-middle pr-2">draw</span>Text</div>
+                    <div><span class="material-symbols-outlined align-middle pr-2">mic</span>Audio</div>
+                    <div><span class="material-symbols-outlined align-middle pr-2">image</span>Image</div>
+                </div>
             </div>
         </div>
 
@@ -39,7 +44,11 @@ export default {
                 <div class="text-xl grow">
                     {{ entry.title }}
                 </div>
-                <div>
+                <div v-if="showEdit" class="flex-col">
+                    <div>Edit</div>
+                    <div>Delete</div>
+                </div>
+                <div @click="showEdit = !showEdit">
                     <span class="material-symbols-outlined w-8 h-8 hover:bg-sky-200 cursor-pointer">
                         more_vert
                     </span>
