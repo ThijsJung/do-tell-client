dc95a0ca3bf411a4c35fdb23b9c67b950eb9babb
Thijs Jung <thijs.jung@gmail.com>
2023-02-01 17:01:28 +0100

    Move to pages structure.

--- app.vue
+++ app.vue
@@ -16,30 +16,6 @@ export default {
     <header class="text-2xl p-3">
       Do Tell
     </header>
-    <div class="flex px-3 py-2">
-      <div class="grow">All journals</div>
-      <div>
-        <button class="w-20 h-8 hover:bg-sky-200 cursor-pointer">
-          <span class="material-symbols-outlined align-middle">
-            add
-          </span>New
-        </button>
-      </div>
-    </div>
-    <article class="p-3 m-2 flex flex-col " v-for="journal in journals">
-      <div class="flex">
-        <div class="text-xl grow">
-          {{ journal.name }}
-        </div>
-        <div>
-          <span class="material-symbols-outlined w-8 h-8 hover:bg-sky-200 cursor-pointer">
-            more_vert
-          </span>
-        </div>
-      </div>
-      <div>
-        {{ journal.description }}
-      </div>
-    </article>
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<script lang="ts">
+export default {
+    data() {
+        return {
+            journals: [
+                { id: 1, name: "Espanol", description: "Un sitio done dejar todos mis pensamientos espanoles." },
+                { id: 2, name: "Dreams", description: "An overview of my subconsciousness, as presented to me during the night time." }
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="flex flex-col px-3 py-2">
+        <div class="flex flex-row">
+            <div class="grow">All journals</div>
+            <div>
+                <button class="w-20 h-8 hover:bg-sky-200 cursor-pointer">
+                    <span class="material-symbols-outlined align-middle">
+                        add
+                    </span>New
+                </button>
+            </div>
+        </div>
+        <article class="p-3 m-2 flex flex-col " v-for="journal in journals"
+            @click="$router.push(`/journal/${journal.id}`)">
+            <div class="flex">
+                <div class="text-xl grow">
+                    {{ journal.name }}
+                </div>
+                <div>
+                    <span class="material-symbols-outlined w-8 h-8 hover:bg-sky-200 cursor-pointer">
+                        more_vert
+                    </span>
+                </div>
+            </div>
+            <div>
+                {{ journal.description }}
+            </div>
+        </article>
+    </div>
+</template>
