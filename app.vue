415affe119a6c0e4042b586dde330dd57517a00d
Thijs Jung <thijs.jung@gmail.com>
2023-02-01 15:29:34 +0100

    CSS for the journals page.

--- app.vue
+++ app.vue
@@ -12,18 +12,34 @@ export default {
 </script>
 
 <template>
-  <div>
-    <header>
+  <div class="bg-black text-white">
+    <header class="text-2xl p-3">
       Do Tell
     </header>
-    <div>
-      <div>All journals</div>
-      <div>New</div>
+    <div class="flex px-3 py-2">
+      <div class="grow">All journals</div>
+      <div>
+        <button class="w-20 h-8 hover:bg-sky-200 cursor-pointer">
+          <span class="material-symbols-outlined align-middle">
+            add
+          </span>New
+        </button>
+      </div>
     </div>
-    <article v-for="journal in journals">
-      {{ journal.name }}
-      {{ journal.description }}
-      <div>Edit menu</div>
+    <article class="p-3 m-2 flex flex-col " v-for="journal in journals">
+      <div class="flex">
+        <div class="text-xl grow">
+          {{ journal.name }}
+        </div>
+        <div>
+          <span class="material-symbols-outlined w-8 h-8 hover:bg-sky-200 cursor-pointer">
+            more_vert
+          </span>
+        </div>
+      </div>
+      <div>
+        {{ journal.description }}
+      </div>
     </article>
   </div>
 </template>
