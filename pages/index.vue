709aa6ea862e71062dc2fe62f7c808124ff36ace
Thijs Jung <thijs.jung@gmail.com>
2023-02-02 15:45:57 +0100

    Fix typo.

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@ export default {
     data() {
         return {
             journals: [
-                { id: 1, name: "Espanol", description: "Un sitio done dejar todos mis pensamientos espanoles." },
+                { id: 1, name: "Espanol", description: "Un sitio donde dejar todos mis pensamientos espanoles." },
                 { id: 2, name: "Dreams", description: "An overview of my subconsciousness, as presented to me during the night time." }
             ]
         }
