478a3abf8ec2a14986b4646fc066b4643c31b1b5
Thijs Jung <thijs.jung@gmail.com>
2023-02-02 18:04:53 +0100

    Better variable names. More fun data examples.

--- pages/journal/[journalId].vue
+++ pages/journal/[journalId].vue
@@ -1,18 +1,29 @@
 <script lang="ts">
 export default {
+    mounted() {
+        this.journalId = this.$route.params.journalId
+        this.entries = this.allEntries[this.journalId]
+    },
     data() {
         return {
-            journal:
-                { id: 1, name: "Espanol", description: "Un sitio done dejar todos mis pensamientos espanoles." },
-            entries: [
-                { id: 1, type: "text", createdAt: "01/02/2023, 14:58", title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" },
-                { id: 2, type: "audio", createdAt: "01/01/2023, 14:58", title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" },
-                { id: 3, type: "text", createdAt: "01/02/2023, 14:58", title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" },
-                { id: 4, type: "text", createdAt: "01/02/2023, 14:58", title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" },
-                { id: 5, type: "text", createdAt: "01/02/2023, 14:58", title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" }
-            ],
+            journalId: 0,
+            journalName: "Espanol",
+            entries: [],
+            allEntries: {
+                1: [
+                    { id: 1, type: "text", createdAt: "01/02/2023, 14:58", title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" },
+                    { id: 2, type: "audio", createdAt: "01/01/2023, 14:58", title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" },
+                    { id: 3, type: "text", createdAt: "01/02/2023, 14:58", title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" },
+                    { id: 4, type: "text", createdAt: "01/02/2023, 14:58", title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" },
+                    { id: 5, type: "text", createdAt: "01/02/2023, 14:58", title: "Hoy en dia", content: "No vas a creer lo que me ha pasado hoy... Caminando por la calle un chico me hacia parar para preguntarme" }
+                ],
+                2: [
+                    { id: 1, type: "text", createdAt: "01/02/2023, 14:58", title: "The maze", content: "I'm walking through a maze. It's green and lush I'm certain I'm getting closer to the center. Somewhere in the distance I hear a bear roar." },
+                    { id: 2, type: "audio", createdAt: "01/01/2023, 14:58", title: "Murder baby", content: "It's coming for me, I know it!" }
+                ]
+            },
             showEdit: false,
-            showNew: false,
+            showNewEntryOptions: false,
             showNewEntry: false
         }
     }
@@ -23,18 +34,18 @@ export default {
     <div class="flex flex-col px-3 py-2">
         <div class="flex flex-row">
             <div class="grow">
-                <NuxtLink class="underline" to="/">All journals</NuxtLink> > {{ journal.name }}
+                <NuxtLink class="underline" to="/">All journals</NuxtLink> > {{ journalName }}
             </div>
             <div>
                 <div class="cursor-pointer rounded border-white border-2 pl-2">
                     <span @click="showNewEntry = true" class="pr-2">New<span
                             class="material-symbols-outlined align-middle pl-2">draw</span></span>
                     <span class="material-symbols-outlined align-middle border-l-2 ml-1 pr-1"
-                        @click="showNew = !showNew">
+                        @click="showNewEntryOptions = !showNewEntryOptions">
                         expand_more
                     </span>
                 </div>
-                <div v-if="showNew" class="flex-col">
+                <div v-if="showNewEntryOptions" class="flex-col py-2">
                     <div><span class="material-symbols-outlined align-middle pr-2"
                             @click="showNewEntry = true">draw</span>Text
                     </div>
@@ -45,7 +56,6 @@ export default {
         </div>
 
         <NewEntry v-if="showNewEntry" @cancel-edit-entry="showNewEntry = false"></NewEntry>
-        
         <EntrySummary v-else v-for="entry in entries" :entry="entry"></EntrySummary>
     </div>
 </template>
