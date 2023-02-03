2c1a3e61ba95c945b97e4c0f7acd0eaff3e2deb6
Thijs Jung <thijs.jung@gmail.com>
2023-02-03 22:46:46 +0100

    Give date some space.

--- components/EntrySummary.vue
+++ components/EntrySummary.vue
@@ -26,7 +26,7 @@ export default {
             <span v-if="entry.content.type === 'text'" class="material-symbols-outlined align-middle">draw</span>
             {{ entry.title }}
         </div>
-        <div class="text-xs">
+        <div class="text-xs my-1">
             {{ epochToDateString(entry.createdAt) }}
         </div>
 
