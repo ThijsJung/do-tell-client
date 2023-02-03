60806e9b3b039c0763649ee381719708ab963c3b
Thijs Jung <thijs.jung@gmail.com>
2023-02-03 13:18:04 +0100

    Move to separate page for an entry.

---
+++ pages/entry/[entryId].vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex flex-col px-3 py-2">
+        <div class="grow">
+                <NuxtLink class="underline" to="/">All journals</NuxtLink> > MAKE ME DYNAMIC
+            </div>
+        <div>Hello? {{ $route.params }}</div>
+    </div>
+</template>
--- pages/journals/entries/[entryId].vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div>Hello? {{ $route.params }}</div>
-</template>
