378d67573f29e413f7995a00b7192bde85a5fe06
Thijs Jung <thijs.jung@gmail.com>
2023-02-09 18:48:45 +0100

    Make audiorecorder available to component.

--- components/AudioContent.vue
+++ components/AudioContent.vue
@@ -10,13 +10,7 @@ export default {
         return {
             transcript: !this.content.transcript ? '' : this.content.transcript,
             isThisThingOn: false,
-            audioSrc: new Blob()
-        }
-    },
-    computed: {
-        audioSrcIsAvailable() {
-            console.log('Updated: audioSrc. Size: ', this.audioSrc.size)
-            return this.audioSrc.size > 0
+            audioRecorder: null
         }
     },
     methods: {
@@ -28,57 +22,81 @@ export default {
             }
             this.$emit('updatedContent', newContent)
         },
+        stopRecording() {
+            console.log(this.audioRecorder.state)
+            if (this.audioRecorder.state !== 'inactive') {
+                this.audioRecorder.stop()
+                this.isThisThingOn = false
+                console.log(this.audioRecorder.state)
+                console.log("recorder stopped")
+            }
+        },
         startRecording() {
-            const stopButton = document.getElementById('stop-button')
-            const player = document.getElementById('player-new')
-            this.isThisThingOn = true
+            const previousAudioPlayer = document.getElementById('audio-player')
+            if (previousAudioPlayer !== null) {
+                previousAudioPlayer.remove()
+            }
+            if (navigator.mediaDevices) {
+                console.log("getUserMedia supported.");
 
-            const handleSuccess = function (stream) {
-                const options = { mimeType: 'audio/webm' };
-                const recordedChunks = [];
-                const mediaRecorder = new MediaRecorder(stream, options);
+                const constraints = { audio: true };
+                let chunks = [];
+                var device = navigator.mediaDevices.getUserMedia(constraints)
+                device.then((stream) => {
+                    this.audioRecorder = new MediaRecorder(stream)
+                    console.log('startRecording, recorder.state', this.audioRecorder.state)
 
-                mediaRecorder.addEventListener('dataavailable', function (e) {
-                    if (e.data.size > 0) recordedChunks.push(e.data);
-                });
+                    this.audioRecorder.onstop = (e) => {
+                        console.log("data available after audioRecorder.stop() called.")
 
-                mediaRecorder.addEventListener('stop', function () {
-                    const newAudioBlob = new Blob(recordedChunks)
-                    player.src = URL.createObjectURL(newAudioBlob)
-                });
+                        const audio = document.createElement("audio")
+                        audio.setAttribute("controls", "")
+                        audio.id = "audio-player"
+                        const audioPlayerContainer = document.getElementById('audio-player-container')
 
-                stopButton.addEventListener('click', function () {
-                    mediaRecorder.stop()
-                });
+                        audioPlayerContainer.appendChild(audio)
 
-                mediaRecorder.start();
-            };
-
-            navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-                .then(handleSuccess);
+                        audio.controls = true
+                        const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" })
+                        chunks = []
+                        const audioURL = URL.createObjectURL(blob)
+                        audio.src = audioURL
+                        console.log("recorder stopped")
+                    }
+                    this.audioRecorder.ondataavailable = (e) => {
+                        chunks.push(e.data);
+                    };
+                    this.audioRecorder.start();
+                    this.isThisThingOn = true
+                    console.log('startRecording, recorder.state', this.audioRecorder.state)
+                }).catch((err) => {
+                    console.error(`The following error occurred: ${err}`);
+                    this.isThisThingOn = false
+                })
+            }
         }
     }
 }
-
 </script>
 
 <template>
     <div>
         <div v-if="isEditViewActive" class="flex flex-col">
             <div class="flex m-5">
-                <button id="start-button" :class="{ 'border-8' : isThisThingOn}" class="mx-2 border-red-600 rounded-full h-20 w-20 border-2"
-                    @click="startRecording"><span class="material-symbols-outlined">
+                <button id="start-button" :class="{ 'bg-red-600': isThisThingOn }"
+                    class="mx-2 border-red-600 rounded-full h-20 w-20 border-4" @click="startRecording"><span
+                        class="material-symbols-outlined">
                         mic
                     </span></button>
                 <div v-show="isThisThingOn">
-                    <button id="stop-button" class="mx-2 border-sky-400 rounded-full h-20 w-20 border-2"><span
+                    <button id="stop-button" @click="stopRecording"
+                        class="mx-2 border-sky-400 rounded-full h-10 w-10 align-middle border-2"><span
                             class="material-symbols-outlined">
                             stop
                         </span></button>
                 </div>
             </div>
-            <!-- v-if="audioSrcIsAvailable" -->
-            <audio id="player-new" class="m-2 justify-center" controls></audio>
+            <div id="audio-player-container"></div>
             <textarea v-model="transcript" class="w-full h-80 border-none bg-black text-white"
                 placeholder="Do tell..."></textarea>
         </div>
