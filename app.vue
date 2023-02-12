fed41245cdc51281207122909afabeeb93c05cea
Thijs Jung <thijs.jung@gmail.com>
2023-02-12 11:39:00 +0100

    Limit size on bigger screens. Add demo mode.

--- app.vue
+++ app.vue
@@ -5,16 +5,17 @@ export default {
       journals: [
         { name: "Espanol", description: "Un sitio done dejar todos mis pensamientos espanoles." },
         { name: "Dreams", description: "An overview of my subconsciousness, as presented to me during the night time." }
-      ]
+      ],
+      demoModeOn: true
     }
   }
 }
 </script>
 
 <template>
-  <div>
-    <header class="text-4xl p-2 text-sky-400">
-      Do Tell
+  <div class="max-w-lg">
+    <header class="flex text-4xl p-2 text-sky-400">
+      <div class="grow">Do tell</div><div v-if="demoModeOn" class="text-xs text-red-600">[DEMO MODE]</div>
     </header>
     <NuxtPage />
   </div>
