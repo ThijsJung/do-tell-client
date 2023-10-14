05f8270a73eb022dd3deb4c62dfdec461941f5ac
Thijs Jung <thijs@scaleflow.com>
2023-10-15 00:27:13 +0200

    Remove resize on edit.

--- components/TextContent.vue
+++ components/TextContent.vue
@@ -11,7 +11,7 @@ export default {
         }
     },
     methods: {
-        updateContent(event){
+        updateContent(event) {
             const newContent = {
                 type: 'text',
                 language: this.content.language,
@@ -29,7 +29,7 @@ export default {
             <p class="py-1" v-for="paragraph in text.split('\n')">{{ paragraph }}</p>
         </div>
         <div v-else>
-            <textarea @focusout="updateContent" v-model="text" class="w-full h-80 border-none bg-black text-white"
+            <textarea @focusout="updateContent" v-model="text" class="w-full h-80 p-0 border-none bg-black text-white"
                 placeholder="Do tell..."></textarea>
         </div>
     </div>
